aae6a399af040d3fea1330b2b90a1bf2b8ecd51e
yunoda <yunoda.commerce@gmail.com>
2022-10-30 13:45:48 +0000

    add abc085b

---
+++ works/abs/abc088b/abc088b.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+a.sort(reverse=True)
+alice = 0
+bob = 0
+for i in range(len(a)):
+    if i % 2 == 0:
+        alice += a[i]
+    else:
+        bob += a[i]
+print(alice - bob)
